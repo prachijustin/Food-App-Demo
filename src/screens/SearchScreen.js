1a3c4a84aae19af0f4828e77837be51df98aeb89
prachijustin <prachi.sharma01sep@gmail.com>
2020-09-23 17:02:29 +0530

    initial commit biolerplate

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>search screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
